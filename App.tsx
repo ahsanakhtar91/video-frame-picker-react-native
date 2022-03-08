84959e6d7e67967025a187ba807f192bc26ff13d
Ahsan Akhtar <ahsanakhtar91@yahoo.com>
2022-03-08 22:13:26 +0500

    Current progress using "react-native-ffmpeg" package

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Asset } from "expo-asset";
+import { RNFFmpeg } from "react-native-ffmpeg";
 
 export default function App() {
+  useEffect(() => {
+    const proceed = async () => {
+      const content = await Asset.loadAsync(require("./src/input-video.mp4"));
+      const videoUri = content[0].localUri;
+
+      RNFFmpeg.execute("-i " + videoUri + " -r 1/1 filename%03d.jpeg").then(
+        (result) => {
+          console.log(`===>  FFmpeg 1 process exited with rc=${result}.`);
+        }
+      );
+    };
+    proceed();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx!</Text>
+      <Text>Video Frame Picker (React Native)</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +30,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
